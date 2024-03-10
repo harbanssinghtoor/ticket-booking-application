26bf557351db13fadc11abca07caac81d1bfa0e9
Harbans Toor <harbanstoor@Toors-Mac.local>
2024-03-10 01:09:03 -0800

    Screenshots added

--- src/main/java/ticket/booking/services/UserBookingService.java
+++ src/main/java/ticket/booking/services/UserBookingService.java
@@ -1,6 +1,5 @@
 package ticket.booking.services;
 
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
